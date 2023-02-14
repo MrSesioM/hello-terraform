f85353a8974cf964a0265808c04de7c5e8bdda26
MrSesioM <moisesms10@hotmail.com>
2023-02-14 16:35:32 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 terraform {
-	required_providers {
-		aws = {
-			source = "hashicorp/aws"
-			version = "~> 4.16"
-		}
-	}
-	required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-	region = "eu-west-1"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "app_server" {
-	ami = ami-0b752bf1df193a6c4
-	instance_type = "t2.micro"
+  ami           = ami-0b752bf1df193a6c4
+  instance_type = "t2.micro"
 
-	tags = {
-		Name = "terraformInstance"
-		APP = "vue2048"
-	}
+  tags = {
+    Name = "terraformInstance"
+    APP  = "vue2048"
+  }
 }
