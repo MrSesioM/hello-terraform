40e88f4df285f7d65f2c755046da5d6a8532d83c
MrSesioM <moisesms10@hotmail.com>
2023-02-15 08:57:54 +0100

    Adding security group, subnet and key pair to our instance

--- main.tf
+++ main.tf
@@ -13,8 +13,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = ami-0b752bf1df193a6c4
+  ami           = "ami-0b752bf1df193a6c4"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-0133c61a0a52088e4"
+  key_name      = "lucatic_key"
+  vpc_security_group_ids = [
+    "sg-01026b2bbaa8626e7",
+  ]
 
   tags = {
     Name = "terraformInstance"
